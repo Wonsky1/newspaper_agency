b0e59760c56f70d4aeb35f787d5c9b4fd8855605
wonsky <pidborskii17@gmail.com>
2023-11-22 14:31:42 +0200

    Added models for project

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(Redactor)
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.topic.name})"
