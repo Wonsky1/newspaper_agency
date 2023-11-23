1e76619cad0a8f7ee718a39551636d008fbb4c41
wonsky <pidborskii17@gmail.com>
2023-11-23 14:06:39 +0200

    Made CRUD views for each model

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -4,25 +4,37 @@ from newspaper.views import (
     NewspaperListView,
     NewspaperDetailView,
     RedactorListView,
+    RedactorCreateView,
+    RedactorUpdateView,
+    RedactorDeleteView,
+    RedactorDetailView,
     TopicListView,
     TopicCreateView,
     TopicDeleteView,
     TopicUpdateView,
-    RedactorDeleteView,
+    TopicDetailView,
     NewspaperDeleteView,
+    NewspaperCreateView,
+    NewspaperUpdateView,
+
 )
 
 urlpatterns = [
     path("", NewspaperListView.as_view(), name="newspaper-list"),
-    path("newspaper/<int:pk>/delete/", NewspaperDeleteView.as_view(), name="newspaper-delete"),
-    path("article/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
+    path("article/<int:pk>/delete/", NewspaperDeleteView.as_view(), name="newspaper-delete"),
+    path("article/<int:pk>/detail", NewspaperDetailView.as_view(), name="newspaper-detail"),
+    path("article/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
+    path("article/<int:pk>/update/", NewspaperUpdateView.as_view(), name="newspaper-update"),
     path("redactors/", RedactorListView.as_view(), name="redactor-list"),
-    path("redactors/delete/<int:pk>/", RedactorDeleteView.as_view(), name="redactor-delete"),
+    path("redactors/<int:pk>/delete/", RedactorDeleteView.as_view(), name="redactor-delete"),
+    path("redactors/create/", RedactorCreateView.as_view(), name="redactor-create"),
+    path("redactors/<int:pk>/update/", RedactorUpdateView.as_view(), name="redactor-update"),
+    path("redactors/<int:pk>/detail/", RedactorDetailView.as_view(), name="redactor-detail"),
     path("topics/", TopicListView.as_view(), name="topic-list"),
     path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
-    path("topics/update/<int:pk>/", TopicUpdateView.as_view(), name="topic-update"),
-    path("topics/delete/<int:pk>/", TopicDeleteView.as_view(), name="topic-delete"),
-
+    path("topics/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update"),
+    path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topic-delete"),
+    path("topics/<int:pk>/detail/", TopicDetailView.as_view(), name="topic-detail"),
 ]
 
 app_name = "newspaper"
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
+from newspaper.forms import RedactorCreationForm, NewspaperForm, RedactorUpdateForm
 from newspaper.models import Newspaper, Redactor, Topic
 
 
@@ -19,6 +19,18 @@ class NewspaperDeleteView(generic.DeleteView):
     model = Newspaper
 
 
+class NewspaperCreateView(generic.CreateView):
+    model = Newspaper
+    form_class = NewspaperForm
+    success_url = reverse_lazy("newspaper:newspaper-list")
+
+
+class NewspaperUpdateView(generic.UpdateView):
+    model = Newspaper
+    form_class = NewspaperForm
+    success_url = reverse_lazy("newspaper:newspaper-list")
+
+
 class RedactorListView(generic.ListView):
     model = Redactor
     paginate_by = 10
@@ -27,6 +39,23 @@ class RedactorListView(generic.ListView):
 class RedactorDeleteView(generic.DeleteView):
     model = Redactor
     success_url = reverse_lazy("newspaper:redactor-list")
+
+
+class RedactorCreateView(generic.CreateView):
+    model = Redactor
+    form_class = RedactorCreationForm
+    success_url = reverse_lazy("newspaper:redactor-list")
+
+
+class RedactorUpdateView(generic.UpdateView):
+    model = Redactor
+    form_class = RedactorUpdateForm
+    success_url = reverse_lazy("newspaper:redactor-list")
+
+
+class RedactorDetailView(generic.DetailView):
+    model = Redactor
+
 
 class TopicListView(generic.ListView):
     model = Topic
@@ -47,3 +76,9 @@ class TopicUpdateView(generic.UpdateView):
 class TopicDeleteView(generic.DeleteView):
     model = Topic
     success_url = reverse_lazy("newspaper:topic-list")
+
+
+class TopicDetailView(generic.DetailView):
+    model = Topic
+
+
