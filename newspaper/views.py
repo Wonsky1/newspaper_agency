b87bab0e3254c2f7ee4924b315cd642cc674daa4
wonsky <pidborskii17@gmail.com>
2023-11-23 18:09:27 +0200

    Implemented search fields for redactors annd topics

--- newspaper/views.py
+++ newspaper/views.py
@@ -104,7 +104,7 @@ class TopicListView(generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(TopicListView, self).get_context_data(**kwargs)
         name = self.request.GET.get("name", "")
-        context["search_form"] = TopicListView(
+        context["search_form"] = TopicSearchForm(
             initial={"name": name}
         )
 
