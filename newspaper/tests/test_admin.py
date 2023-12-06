fb8b7e9eadb069cd9ffe0430daa4491c70c78f42
wonsky <pidborskii17@gmail.com>
2023-12-07 01:58:56 +0200

    Added parametrizing for not duplicating code in test_admin.py

--- newspaper/tests/test_admin.py
+++ newspaper/tests/test_admin.py
@@ -6,6 +6,9 @@ from newspaper.models import Topic, Newspaper
 
 
 class AdminSiteTests(TestCase):
+    NEWSPAPER_CHANGE_LIST_URL = "admin:newspaper_newspaper_changelist"
+    REDACTOR_CHANGE_URL = "admin:newspaper_redactor_change"
+
     def setUp(self) -> None:
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
@@ -22,30 +25,30 @@ class AdminSiteTests(TestCase):
 
     def test_redactor_admin_page(self):
         response = self.client.get(
-            reverse("admin:newspaper_redactor_changelist")
+            reverse(self.NEWSPAPER_CHANGE_LIST_URL)
         )
         self.assertEqual(response.status_code, 200)
 
     def test_newspaper_admin_page(self):
         response = self.client.get(
-            reverse("admin:newspaper_newspaper_changelist")
+            reverse(self.NEWSPAPER_CHANGE_LIST_URL)
         )
         self.assertEqual(response.status_code, 200)
 
     def test_topic_admin_page(self):
-        url = reverse("admin:newspaper_topic_changelist")
+        url = reverse(self.NEWSPAPER_CHANGE_LIST_URL)
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_redactor_years_of_experience_listed(self) -> None:
-        url = reverse("admin:newspaper_redactor_changelist")
+        url = reverse(self.NEWSPAPER_CHANGE_LIST_URL)
         response = self.client.get(url)
 
         self.assertContains(response, str(self.redactor.years_of_experience))
 
     def test_redactor_detail_years_of_experience_listed(self):
         url = reverse(
-            "admin:newspaper_redactor_change",
+            self.REDACTOR_CHANGE_URL,
             args=[self.redactor.pk]
         )
         response = self.client.get(url)
@@ -53,13 +56,13 @@ class AdminSiteTests(TestCase):
         self.assertContains(response, str(self.redactor.years_of_experience))
 
     def test_newspaper_admin_search(self):
-        url = reverse("admin:newspaper_newspaper_changelist")
+        url = reverse(self.NEWSPAPER_CHANGE_LIST_URL)
         response = self.client.get(url, {"q": "Test Newspaper"})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Newspaper")
 
     def test_newspaper_admin_filter(self):
-        url = reverse("admin:newspaper_newspaper_changelist")
+        url = reverse(self.NEWSPAPER_CHANGE_LIST_URL)
         response = self.client.get(url, {"topic__id__exact": self.topic.id})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Newspaper")
